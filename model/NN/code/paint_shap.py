17362a9764ead96ed34f88d90cec2d179360d0ee
lihr1008 <100561118+lihr1008@users.noreply.github.com>
2025-04-07 20:52:31 +0800

    Update paint_shap.py

--- model/NN/code/paint_shap.py
+++ model/NN/code/paint_shap.py
@@ -8,11 +8,9 @@ excel = '../results/new_shap_data.xlsx'
 shap_data = pd.read_excel(excel,sheet_name='shap')
 shap_values = shap_data.values
 x_val=pd.read_excel(excel,sheet_name='x_val')
-x_explain = x_val.values      # 选择需要解释的样本（例如验证集全部）
+x_explain = x_val.values 
 
-# 绘制summary plot，展示各特征对预测结果的影响
 plt.figure()
 shap.summary_plot(shap_values, x_explain, feature_names=x_val.columns,show=False)
-# 获取当前figure并保存为png格式，dpi可根据需要调整
 plt.savefig("../results/new_shap_summary_plot.png", dpi=300, bbox_inches='tight')
 
