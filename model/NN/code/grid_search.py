31c0251df331c074267fc91cb409361ac9879551
lihr1008 <100561118+lihr1008@users.noreply.github.com>
2025-06-19 16:45:13 +0800

    Update grid_search.py

--- model/NN/code/grid_search.py
+++ model/NN/code/grid_search.py
@@ -1,9 +1,57 @@
-from train import premodel,model
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import torch
+import torch.nn as nn
+from torch.nn import functional as F
+from torch.nn import Module
+import torch.optim as optim
+
+class Model(Module):
+    def __init__(self):
+        super(Model,self).__init__()
+        self.Layer1 = nn.Linear(in_features=5,out_features=512)
+        self.Layer2 = nn.Linear(in_features=512,out_features=512)
+        self.Layer3 = nn.Linear(in_features=512,out_features=4)
+
+    def forward(self,x):
+        x = F.relu(self.Layer1(x))
+        x = F.relu(self.Layer2(x))
+        x = self.Layer3(x)
+
+        return x
+
+model = torch.load('PBA-ml-pretrain_model.pkl')
+state_dict = model.state_dict()
+torch.save(state_dict, 'PBA-ml-pretrain_model_weights.pth')
+premodel = Model()
+premodel.load_state_dict(torch.load('PBA-ml-pretrain_model_weights.pth'))
+premodel.eval()
+
+
+class Model(Module):
+    def __init__(self):
+        super(Model,self).__init__()
+        self.Layer1 = nn.Linear(in_features=4,out_features=512)
+        self.Layer2 = nn.Linear(in_features=512,out_features=512)
+        self.Layer3 = nn.Linear(in_features=512,out_features=512)
+        self.Layer4 = nn.Linear(in_features=512,out_features=1)
+
+    def forward(self,x):
+        x = F.relu(self.Layer1(x))
+        x = F.relu(self.Layer2(x))
+        x = F.relu(self.Layer3(x))
+        x = self.Layer4(x)
+
+        return x
+
+trained_model = torch.load('PBA-ml-train_model.pth')
+state_dict = trained_model.state_dict()
+model = Model()
+torch.save(state_dict, 'train_model_weights.pth')
+model.load_state_dict(torch.load('train_model_weights.pth'))
+model.eval()
 
 excel = '../../data/experiment.xlsx'
 x = pd.read_excel(excel, sheet_name='metals')
