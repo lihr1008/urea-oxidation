3ae83cec8222316db170197c11345e5a330c2623
lihr1008 <100561118+lihr1008@users.noreply.github.com>
2025-07-17 15:29:07 +0800

    Update paint_shap.py

--- model/NN/code/paint_shap.py
+++ model/NN/code/paint_shap.py
@@ -11,6 +11,49 @@ x_val=pd.read_excel(excel,sheet_name='x_val')
 x_explain = x_val.values 
 
 plt.figure()
-shap.summary_plot(shap_values, x_explain, feature_names=x_val.columns,show=False)
-plt.savefig("../results/new_shap_summary_plot.png", dpi=300, bbox_inches='tight')
+shap.summary_plot(
+    shap_values,
+    x_explain,
+    feature_names=x_val.columns,
+    show=False 
+)
 
+fig = plt.gcf()  
+axes = fig.axes  
+
+ax_main = axes[0]
+# x轴标签（SHAP value (impact on model output)）
+ax_main.set_xlabel(
+    ax_main.get_xlabel(),
+    fontweight='bold',
+    fontsize=12 
+)
+# y轴特征名称（Mn、Co、Zn等）
+for tick in ax_main.get_yticklabels():
+    tick.set_fontweight('bold')
+    tick.set_fontsize(12)  
+
+
+for tick in ax_main.get_xticklabels():
+    tick.set_fontweight('bold')
+    tick.set_fontsize(12) 
+
+if len(axes) > 1: 
+    ax_colorbar = axes[1]
+    ax_colorbar.set_ylabel(
+        ax_colorbar.get_ylabel(),
+        fontweight='bold',
+        fontsize=12
+    )
+
+    for tick in ax_colorbar.get_yticklabels():
+        tick.set_fontweight('bold')
+        tick.set_fontsize(12)
+
+
+plt.savefig(
+    "../results/new_shap_summary_plot.png",
+    dpi=300,
+    bbox_inches='tight' 
+)
+plt.close() 
